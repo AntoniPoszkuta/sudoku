f7a2d6a2cb98f5ba941f564d47fa2b0e2bc10992
antek <test1159@wp.pl>
2026-04-14 15:56:17 +0200

    dodano funkcje sprawdzajace poprawnosc kolumn i wierszy

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int sudoku[9][9] = {0};
 
-void wyswietl(int tab[9][9]) {
+bool check_row(int tab[9][9], int x, int y, int target) {
+    for (int i = 0; i < 9; i++) {
+        if (i == x) continue;
+        if (tab[y][i] == target) return false;
+    }
+    return true;
+}
+
+bool check_col(int tab[9][9], int x, int y, int target) {
+    for (int i = 0; i < 9; i++) {
+        if (i == y) continue;
+        if (tab[i][x] == target) return false;
+    }
+    return true;
+}
+
+void display_grid(int tab[9][9]) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (j % 3 == 0 and j!= 0) {
